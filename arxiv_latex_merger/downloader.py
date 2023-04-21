a070fc49b1974e0a65602c7545eca3d1d4371af2
iokarkan <iokarkan@github.com>
2023-04-21 14:34:35 +0300

    integrating into arxiv module

--- arxiv_latex_merger/downloader.py
+++ arxiv_latex_merger/downloader.py
@@ -1,36 +1,33 @@
 import os
 import tarfile
-import urllib.request
+import arxiv
 from pathlib import Path
 from tqdm import tqdm
 
 def download_arxiv_source_files(arxiv_code):
-    url = f"https://arxiv.org/e-print/{arxiv_code}"
-    file_name = f"{arxiv_code}.tar"
     output_dir = arxiv_code
 
     # Create the output directory if it doesn't exist
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    # Download the tar file
-    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-        content_length = int(response.getheader('content-length'))
-        progress_bar = tqdm(total=content_length, unit='B', unit_scale=True, desc=f'Downloading source files for {arxiv_code}')
-        downloaded_size = 0
-        while True:
-            chunk = response.read(1024)
-            if not chunk:
-                break
-            out_file.write(chunk)
-            downloaded_size += len(chunk)
-            progress_bar.update(len(chunk))
-        progress_bar.close()
+    # Use arxiv API to get the paper object
+    papers = arxiv.Search(id_list=[arxiv_code]).results()
+    paper = next(papers)
+
+    # Download the source files using the download_source method
+    paper.download_source(dirpath=output_dir, filename=f"{arxiv_code}.tar.gz")
 
     # Extract the tar file to the output directory
-    with tarfile.open(file_name) as tar:
-        tar.extractall(output_dir)
+    tar_file = os.path.join(output_dir, f"{arxiv_code}.tar.gz")
+    with tarfile.open(tar_file, "r:gz") as tar:
+        members = tar.getmembers()
+        progress_bar = tqdm(total=len(members), unit="file", desc=f"Extracting source files for {arxiv_code}")
+        for member in members:
+            tar.extract(member, output_dir)
+            progress_bar.update(1)
+        progress_bar.close()
 
-    # Delete the tar file
-    os.remove(file_name)
+    # Remove the tar file
+    os.remove(tar_file)
 
-    print(f'Successfully downloaded and extracted source files to {output_dir} directory')
+    print(f"Successfully downloaded and extracted source files to {output_dir} directory")
--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='arxiv-latex-merger',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'arxiv==1.3.0',
+    ],
     entry_points={
         'console_scripts': [
             'arxiv-latex-merger = arxiv_latex_merger.cli:cli',
