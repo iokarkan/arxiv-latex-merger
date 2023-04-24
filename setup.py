011a419af5b8ca8d98e6df05e66791aa313bbfa9
iokarkan <iokarkan@github.com>
2023-04-24 18:49:02 +0300

    bump version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-latex-merger',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'arxiv==1.3.0',
