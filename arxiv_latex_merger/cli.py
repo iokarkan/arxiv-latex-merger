d3abcbe9bdd6a44617dd51561416bd7d67f31301
iokarkan <iokarkan@github.com>
2023-04-26 15:06:50 +0300

    small re-arrangement of cli, added remove_src option

--- arxiv_latex_merger/cli.py
+++ arxiv_latex_merger/cli.py
@@ -4,15 +4,17 @@ __version__='0.2.0'
 __author__='iokarkan'
 
 import argparse
+from pathlib import Path
+import shutil
 from .merger import merge_tex_files, find_main_tex_file
 from .downloader import download_arxiv_source_files, download_random_arxiv_papers
 from .demacro import LatexDemacro
 
-def main(arxiv_codes, n_random=None, demacro=False):
+def main(args):
     print(f"arxiv_latex_merger {__version__}")
 
-    if arxiv_codes:
-        for code in arxiv_codes:
+    if args.arxiv_codes:
+        for code in args.arxiv_codes:
             download_arxiv_source_files(code)
 
             main_tex_path = find_main_tex_file(code)
@@ -25,32 +27,41 @@ def main(arxiv_codes, n_random=None, demacro=False):
 
             print(f'Merged .tex file saved to {output_tex_path}')
     else:
-        print(f"Downloading {n_random} random arXiv paper(s)...")
-        arxiv_codes = download_random_arxiv_papers(n_random)
+        print(f"Downloading {args.n_random} random arXiv paper(s)...")
+        args.arxiv_codes = download_random_arxiv_papers(args.n_random)
 
-    for code in arxiv_codes:
+    for code in args.arxiv_codes:
         main_tex_path = find_main_tex_file(code)
         output_tex_path = f'{code}_merged.tex'
 
-        merged_tex_content = merge_tex_files(main_tex_path)
+        try:
+            merged_tex_content = merge_tex_files(main_tex_path)
+            with open(f"{code}_merged.tex", "w") as output_file:
+                output_file.write(merged_tex_content)
+            print(f"{code} file saved to {code}_merged.tex.")
 
-        with open(f"{code}_merged.tex", 'w') as output_file:
-            output_file.write(merged_tex_content)
-        
-        print(f'Merged .tex file saved to {output_tex_path}')
+            if args.remove_src:
+                shutil.rmtree(Path(f"./{code}"))
+            
+        except Exception as e:
+            print(f"Could not merge files for {code}: {e}")
 
-        if demacro:
+        if args.demacro:
             print(f"WARNING: Using experimental 'demacro' processing...")
             input_tex_path = output_tex_path
             output_tex_path = f"{code}_merged_clean.tex"
-            demacro = LatexDemacro(inp=input_tex_path, out=output_tex_path)
-            merged_clean = demacro.process()
+            demacro_f = LatexDemacro(inp=input_tex_path, out=output_tex_path)
 
-            with open(f"{code}_merged_clean.tex", "w") as output_file:
-                output_file.write(merged_clean)
+            try:
+                merged_clean = demacro_f.process()
+                with open(f"{code}_merged_clean.tex", "w") as output_file:
+                    output_file.write(merged_clean)
+                print(f"{code} file saved to {output_tex_path}.")
+
+            except Exception as e:
+                print(f"Could not demacro files for {code}: {e}")
             
-            print(f"Demacro'd & merged .tex file saved to {output_tex_path}")
-
+        print(f"Finished processing {code}.")
 
 
 def cli():
@@ -58,5 +69,6 @@ def cli():
     parser.add_argument('--arxiv_codes', nargs='+', default=[], help='The arXiv code(s) for the paper(s).')
     parser.add_argument('--n_random', default=1, help='Fetch n random papers.')
     parser.add_argument('--demacro', action='store_true', default=False, help='(Experimental/Buggy) Attempt to de-macro custom commands defined in the merged file.')
+    parser.add_argument('--remove_src', action='store_true', default=False, help='Remove source folder after successful merging.')
     args = parser.parse_args()
-    main(args.arxiv_codes, n_random=args.n_random, demacro=args.demacro)
+    main(args)
